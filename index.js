2f653f9fbd2e92b6f0dcd3bb32b6c99888113307
Maëlle RABOUAN <maelle@MacBook-Pro-de-Maelle.local>
2021-10-22 09:39:05 +0200

    test

--- index.js
+++ index.js
@@ -50,4 +50,4 @@ app.post('/api/musics', (req, res)=>{
 })
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, ()=> console.log(`Server is runing : port ${PORT}`))
+app.listen(PORT, ()=> console.log(`Server is runing , port ${PORT}`))
